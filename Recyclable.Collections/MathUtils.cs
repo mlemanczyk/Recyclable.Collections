bd04f1093159622c6ab2cfa58d37268163c1cfb9
mlemanczyk <marek@lemanczyk.eu>
2023-03-26 23:40:13 +0200

    Tiny cleanup

--- Recyclable.Collections/MathUtils.cs
+++ Recyclable.Collections/MathUtils.cs
@@ -4,7 +4,7 @@ namespace Recyclable.Collections
 {
 	public static class MathUtils
 	{
-		private static long[] Pow2 = new long[]
+		private static readonly long[] Pow2 = new long[]
 		{
 			1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
 			32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304,
