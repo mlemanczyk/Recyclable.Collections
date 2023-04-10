24a14f810bcd4ad9f3432aa099133dcaefe4b2af
mlemanczyk <marek@lemanczyk.eu>
2023-04-11 00:16:05 +0200

    Additional benchmarks added. One-off quick smoke test added.

--- Recyclable.Collections.Benchmarks/Program.cs
+++ Recyclable.Collections.Benchmarks/Program.cs
@@ -29,20 +29,25 @@ internal class Program
 	{
 		var benchmark = new RecyclableCollectionsBenchmarks();
 		benchmark.Setup();
-		foreach (var _ in Enumerable.Range(1, 1000))
-		{
-			//benchmark.RecyclableList_Contains();
-			//benchmark.RecyclableArrayList_AddRange_WithCapacity();
-			//benchmark.RecyclableArrayList_Add_WithCapacity();
+		//foreach (var _ in Enumerable.Range(1, 1000))
+		//{
+		//benchmark.RecyclableList_Contains();
+		//benchmark.RecyclableArrayList_AddRange_WithCapacity();
+		//benchmark.RecyclableArrayList_Add_WithCapacity();
 
-			//benchmark.RecyclableList_Create();
-			//benchmark.RecyclableList_Create_WithCapacity();
-			//benchmark.RecyclableList_Add();
-			//benchmark.RecyclableList_Add_WithCapacity();
-			//benchmark.RecyclableList_Contains_LastItems();
-			//benchmark.RecyclableList_IndexOf_FirstItems();
-			benchmark.RecyclableList_AddRangeWhenSourceIsList();
-		}
+		//benchmark.RecyclableList_Create();
+		//benchmark.RecyclableList_Create_WithCapacity();
+		//benchmark.RecyclableList_Add();
+		//benchmark.RecyclableList_Add_WithCapacity();
+		//benchmark.RecyclableList_Contains_LastItems();
+		//benchmark.RecyclableList_IndexOf_FirstItems();
+		//benchmark.RecyclableList_AddRangeWhenSourceIsList();
+		//benchmark.RecyclableList_Remove_FirstItems();
+		//benchmark.RecyclableList_RemoveAt_LastItems();
+		//benchmark.RecyclableList_EnsureCapacity_New_ByBlockSize();
+		benchmark.RecyclableList_EnsureCapacity_ByPowOf2();
+		//benchmark.List_RemoveAt_LastItems();
+		//}
 
 		benchmark.Cleanup();
 	}
@@ -52,6 +57,23 @@ internal class Program
 		BenchmarkRunner.Run<RecyclableCollectionsBenchmarks>(
 			ManualConfig.Create(DefaultConfig.Instance).WithOptions(ConfigOptions.DisableOptimizationsValidator | ConfigOptions.JoinSummary)
 		);
+
+		//var pool = ArrayPool<object>.Shared;
+		//for (long bufferSize = int.MaxValue / 2; bufferSize <= int.MaxValue; bufferSize += 100_000_000)
+		//{
+		//	try
+		//	{
+		//		Console.WriteLine($"Allocating {bufferSize}");
+		//		var arr = pool.Rent((int)bufferSize);
+		//		pool.Return(arr);
+		//		GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true);
+		//	}
+		//	catch (Exception)
+		//	{
+		//		Console.WriteLine($"Error during array creation. {nameof(bufferSize)} = {bufferSize}");
+		//		throw;
+		//	}
+		//}
 
 		//RunBenchmark();
 
