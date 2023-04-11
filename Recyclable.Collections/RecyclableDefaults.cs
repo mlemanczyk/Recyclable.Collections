595566d0b999a20ad8689ac7f71c401faa36d595
mlemanczyk <marek@lemanczyk.eu>
2023-04-11 20:31:34 +0200

    The current maximum pooled block size, from testing, added.

--- Recyclable.Collections/RecyclableDefaults.cs
+++ Recyclable.Collections/RecyclableDefaults.cs
@@ -5,5 +5,6 @@
 		public const int BlockSize = 16_384;
 		public const int Capacity = 1;
 		public const int MinPooledArrayLength = 128;
+		public const int MaxPooledBlockSize = 2_147_483_591;
 	}
 }
