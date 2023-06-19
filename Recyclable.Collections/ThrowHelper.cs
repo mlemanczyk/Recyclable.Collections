85aac1dad65554efcc1ebeaf4f6ec4ad09625e66
mlemanczyk <marek@lemanczyk.eu>
2023-06-20 00:41:28 +0200

    Contracts added to ThrowHelper.

--- Recyclable.Collections/ThrowHelper.cs
+++ Recyclable.Collections/ThrowHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace Recyclable.Collections
+﻿using System.Diagnostics.Contracts;
+
+namespace Recyclable.Collections
 {
 	internal static class ThrowHelper
 	{
@@ -6,18 +8,30 @@
 		/// This is a helper method to throw <see cref="InvalidOperationException"/>, thrown when the collection was modified during enumeration.
 		/// </summary>
 		/// <exception cref="InvalidOperationException">Always</exception>
-		public static void ThrowListModifiedException() => throw new InvalidOperationException("Can't move to the next item, because the collection was modified. You must restart the enumeration by calling Reset(), if you want to enumerate the collection again");
+		public static void ThrowListModifiedException()
+		{
+			Contract.EnsuresOnThrow<InvalidOperationException>(false);
+			throw new InvalidOperationException("Can't move to the next item, because the collection was modified. You must restart the enumeration by calling Reset(), if you want to enumerate the collection again");
+		}
 
 		/// <summary>
 		/// This is a helper method to throw <see cref="InvalidOperationException"/> with the given message.
 		/// </summary>
 		/// <exception cref="InvalidOperationException">Always</exception>
-		public static void ThrowInvalidOperationException(in string message) => throw new InvalidOperationException(message);
+		public static void ThrowInvalidOperationException(in string message)
+		{
+			Contract.EnsuresOnThrow<InvalidOperationException>(false);
+			throw new InvalidOperationException(message);
+		}
 
 		/// <summary>
 		/// This is a helper method to throw <see cref="ArgumentOutOfRangeException"/> with the given message.
 		/// </summary>
 		/// <exception cref="ArgumentOutOfRangeException">Always</exception>
-		public static void ThrowArgumentOutOfRangeException(in string argumentName, in string message) => throw new ArgumentOutOfRangeException(argumentName, message);
+		public static void ThrowArgumentOutOfRangeException(in string argumentName, in string message)
+		{
+			Contract.EnsuresOnThrow<ArgumentOutOfRangeException>(false);
+			throw new ArgumentOutOfRangeException(argumentName, message);
+		}
 	}
 }
