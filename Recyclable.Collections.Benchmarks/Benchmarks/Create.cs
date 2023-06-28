5d36a081389b8d277e865679dc46f5bb0457ae68
mlemanczyk <marek@lemanczyk.eu>
2023-06-28 23:25:06 +0200

    Problem with running RecyclableCollectionsBenchmarks.Create fixed. Methods made static.

--- Recyclable.Collections.Benchmarks/Benchmarks/Create.cs
+++ Recyclable.Collections.Benchmarks/Benchmarks/Create.cs
@@ -4,25 +4,25 @@ namespace Recyclable.Collections.Benchmarks
 {
 	public partial class RecyclableCollectionsBenchmarks
 	{
-		public static void List_Create()
+		public void List_Create()
 		{
 			var list = new List<long>();
 			DoNothing(list);
 		}
 
-		public static void PooledList_Create()
+		public void PooledList_Create()
 		{
 			using var list = new PooledList<long>(ClearMode.Auto);
 			DoNothing(list);
 		}
 
-		public static void RecyclableList_Create()
+		public void RecyclableList_Create()
 		{
 			using var list = new RecyclableList<long>();
 			DoNothing(list);
 		}
 
-		public static void RecyclableLongList_Create()
+		public void RecyclableLongList_Create()
 		{
 			using var list = new RecyclableLongList<long>();
 			DoNothing(list);
